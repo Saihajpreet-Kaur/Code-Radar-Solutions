cdeff3d1fdea459e6644d5e7b03c5e60f605072e
Saihajpreet-Kaur <saihajpreet0703.becse24@chitkara.edu.in>
2025-01-31 15:42:10 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char a;
+    scanf("%c",&a);
+
+    if(a == 'A'){
+        printf("Excellent")
+    }else if(a=='B'){
+        printf("Good")
+    }else if(a=='C')
+    return 0;
+}
