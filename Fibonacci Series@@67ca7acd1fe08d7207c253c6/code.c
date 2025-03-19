60dbe4aaea9fca8cf0405fe647cf327428a08e74
Saihajpreet-Kaur <saihajpreet0703.becse24@chitkara.edu.in>
2025-03-19 23:56:59 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Function to print Fibonacci Series
+void fibonacciSeries(int n) {
+    int a = 0, b = 1, next;
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", a);
+        if (i < n - 1) {
+            printf(" ");  // Space between numbers, but no trailing space
+        }
+
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    printf("\n");  // Newline at the end
+}
