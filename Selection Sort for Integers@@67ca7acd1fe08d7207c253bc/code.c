d122d3ef2771327ce7ba20afeb52bed3a01ae1d2
Saihajpreet-Kaur <saihajpreet0703.becse24@chitkara.edu.in>
2025-03-20 00:26:42 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -17,7 +17,7 @@ void printArray(int arr[],int n){
     for(int i=0;i<n;i++){
         printf("%d",arr[i]);
         if(i<n-1){
-            printf(" ")
+            printf(" ");
         }
     }
     printf("\n");
