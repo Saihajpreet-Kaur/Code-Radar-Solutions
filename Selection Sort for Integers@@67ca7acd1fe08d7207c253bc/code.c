aed83f6915b19f9c95fddf6102ead16819137f53
Saihajpreet-Kaur <saihajpreet0703.becse24@chitkara.edu.in>
2025-03-20 00:26:27 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,29 +1,24 @@
-// Your code here...
-#include <stdio.h>
-
-// Function to perform Selection Sort
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int min_index = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_index]) {
-                min_index = j;
+#include<stdio.h>
+void selectionSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int min_index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_index]){
+                min_index=j;
             }
         }
-        // Swap arr[i] and arr[min_index]
         int temp = arr[i];
-        arr[i] = arr[min_index];
-        arr[min_index] = temp;
+        arr[i]=arr[min_index];
+        arr[min_index]=temp;
     }
 }
 
-// Function to print the sorted array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d", arr[i]);
-        if (i < n - 1) {
-            printf(" ");  // Space between numbers but no trailing space
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+        if(i<n-1){
+            printf(" ")
         }
     }
-    printf("\n");  // Newline at the end
+    printf("\n");
 }
