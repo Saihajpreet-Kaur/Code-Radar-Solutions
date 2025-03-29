29db00831340010b6932ad709542685441c0f347
Saihajpreet-Kaur <saihajpreet0703.becse24@chitkara.edu.in>
2025-03-29 23:53:30 +0530

    Solution to Check if an Array is Monotonic

--- Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -5,16 +5,19 @@ int main(){
 
     int arr[N];
     for(int i=0;i<N;i++){
-        scanf("%d",arr[i]);
+        scanf("%d",&arr[i]);
     }
 
-    int isMonotonic=0;
+    int dec =1,inc=1;
     for(int i=0;i<N-1;i++){
-        if(arr[i]>arr[i+1] || arr[i]<arr[i+1]){
-            isMonotonic=1;
+        if(arr[i]>arr[i+1]){
+            inc=0;
+        }
+        if(arr[i]<arr[i+1]){
+            dec=0;
         }
     }
-    if(isMonotonic){
+    if(inc || dec){
         printf("YES\n");
     }else{
         printf("NO\n");
