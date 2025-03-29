e7577f7227f503639e13828a09d87254ca519fed
Saihajpreet-Kaur <saihajpreet0703.becse24@chitkara.edu.in>
2025-03-29 22:43:20 +0530

    Solution to Count the Number of Prime Numbers in an Array

--- Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
+++ Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
@@ -1,28 +1,64 @@
-#include<stdio.h>
-int main(){
+// #include<stdio.h>
+// int main(){
+//     int N;
+//     scanf("%d",&N);
+
+//     int arr[N];
+//     for(int i=0;i<N;i++){
+//         scanf("%d",&arr[i]);
+//     }
+
+//     int count=0;
+//     int isPrime=1;
+//     for(int i=0;i<N;i++){
+//         if(arr[i]<=0){
+//             isPrime=0;
+//         }
+//         for(int j=0;j*j<=N;j++){
+//             if(arr[i]%j==0){
+//                 isPrime=0;
+//             }
+//         }
+//         if(isPrime){
+//             count++;
+//         }
+//     }
+//     printf("%d",count);
+//     return 0;
+// }
+#include <stdio.h>
+
+int main() {
     int N;
-    scanf("%d",&N);
+    scanf("%d", &N);
 
     int arr[N];
-    for(int i=0;i<N;i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    int count=0;
-    int isPrime=1;
-    for(int i=0;i<N;i++){
-        if(arr[i]<=0){
-            isPrime=0;
-        }
-        for(int j=0;j*j<=N;j++){
-            if(arr[i]%j==0){
-                isPrime=0;
+    int count = 0;
+
+    for (int i = 0; i < N; i++) {
+        int num = arr[i];
+        int isPrime = 1;
+
+        if (num <= 1) {
+            isPrime = 0;
+        } else {
+            for (int j = 2; j * j <= num; j++) {
+                if (num % j == 0) {
+                    isPrime = 0;
+                    break;
+                }
             }
         }
-        if(isPrime){
+
+        if (isPrime) {
             count++;
         }
     }
-    printf("%d",count);
+
+    printf("%d\n", count);
     return 0;
 }
